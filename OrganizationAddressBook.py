fe37fc5f1c03dc06a1b26458249b6ad3d3da06a9
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-17 11:37:46 +0000

    Setup configuration and database connection

--- OrganizationAddressBook.py
+++ OrganizationAddressBook.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+import os
+import sqlite3
+from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'OrganizationAddressBook.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('OAB_SETTINGS', silent=True)
+
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run()
