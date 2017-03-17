c6728ba22b484f45b77e57bb001300b6565b9414
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-17 14:57:27 +0000

    Add route function for login and logout

--- OrganizationAddressBook/OrganizationAddressBook.py
+++ OrganizationAddressBook/OrganizationAddressBook.py
@@ -67,5 +67,26 @@ def add_entry():
     return redirect(url_for('contacts_list'))
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('contacts_list'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('contacts_list'))
+
 if __name__ == '__main__':
     app.run()
